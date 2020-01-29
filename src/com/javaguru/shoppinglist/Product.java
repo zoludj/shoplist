abe0083a0795ce91af4ef09d73f36c03864e48c6
zoludj <galogen112@gmail.com>
2020-01-29 13:17:53 +0200

    start shopping validator Method CreateProduct Method toString

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -39,4 +39,16 @@ public class Product {
     public BigDecimal getPrice() { return price; }
 
     public void setPrice(BigDecimal price) { this.price = price; }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", discount=" + discount +
+                ", info='" + info + '\'' +
+                '}';
+    }
 }
