2b2fb67ce75bbf619eb478b2c4655e62a1a4b8e8
zoludj <galogen112@gmail.com>
2020-01-29 12:12:58 +0200

    start shopping validator Method CreateProduct

--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -1,5 +1,6 @@
 package com.javaguru.shoppinglist;
 
+import javax.swing.*;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +13,8 @@ class ShoppingListApplication {
         Long productIdSequence = 0L;
         while (true) {
             Scanner scanner = new Scanner(System.in);
+
+
             try {
                 System.out.println("1. Create product");
                 System.out.println("2. Find product by id");
@@ -19,32 +22,48 @@ class ShoppingListApplication {
                 Integer userInput = Integer.valueOf(scanner.nextLine());
                 switch (userInput) {
                     case 1:
-
-
-                        System.out.println("Enter product name: ");
-                        String name = scanner.nextLine();
-                        System.out.println("Enter product price: ");
-                        BigDecimal price = new BigDecimal(scanner.nextLine());
+                        Validator validator = new Validator();
                         Product product = new Product();
-
-                        product.setName(name);
-                        product.setPrice(price);
-                        product.setId(productIdSequence);
-                        productRepository.put(productIdSequence, product);
-                        productIdSequence++;
-                        System.out.println("Result: " + product.getId());
-                    case 2:
-                        System.out.println("Enter product id: ");
-                        long id = scanner.nextLong();
-                        Product findProductResult = productRepository.get(id);
-                        System.out.println(findProductResult);
-                    case 3:
-
-                        return;
+                        createProduct(validator, scanner, product);
+                        System.out.println(product + "created");
                 }
-            } catch (Exception e) {
-                System.out.println("Error! Please try again.");
             }
         }
     }
-}
+
+    private static void createProduct(Validator validator, Scanner scanner, Product product) {
+        System.out.println("Enter product name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter product price: ");
+        BigDecimal price = new BigDecimal(scanner.nextLine());
+        System.out.println("Enter product discount");
+        int discount = Integer.parseInt(scanner.nextLine());
+        if (!validator.validateName(name) || !validator.validateDiscount(discount) || !validator.validatePrice(price)) {
+            System.out.println("Validation failed");
+            createProduct(validator, scanner, product);
+        }
+
+
+        product.setDiscount(discount);
+        product.setName(name);
+        product.setPrice(price);
+        product.setId(productIdSequence);
+        productRepository.put(productIdSequence, product);
+        productIdSequence++;
+        System.out.println("Result: " + product.getId());
+        case 2:
+        System.out.println("Enter product id: ");
+        long id = scanner.nextLong();
+        Product findProductResult = productRepository.get(id);
+        System.out.println(findProductResult);
+        case 3:
+
+        return;
+    }
+} catch(Exception e)
+        System.out.println("Error! Please try again.");
+        {
+
+        }
+
+
